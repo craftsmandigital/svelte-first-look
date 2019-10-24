f73a739b00a868bb1770da000b5abb2a943d7a76
Jon <htcjon10@gmail.com>
2019-10-24 13:42:33 +0200

    Using bulma css framework
    Craated a bulma styled button

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,9 @@
 	}
 </script>
 
+<a class="button is-primary" on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</a>
 <button on:click={handleClick}>
 	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
